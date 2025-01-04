cf8349ede4e3c0f163daca6274f7f238bd6e7417
Kent Liu <kent.liu@rschooltoday.com>
2025-01-04 14:58:06 -0600

    debug

--- model/llama3_qjl.py
+++ model/llama3_qjl.py
@@ -142,7 +142,7 @@ class LlamaAttention_QJL(nn.Module):
         if past_key_value is not None:
             kv_seq_len += past_key_value[-1]
         # cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
-        cos, sin = self.rotary_emb(value_states)
+        cos, sin = self.rotary_emb(value_states, position_ids=kv_seq_len)
 
         query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
         if past_key_value is not None:
